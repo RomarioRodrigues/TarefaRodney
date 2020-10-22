88049ff216bd55d2e619e4260b17b22149ac4067
Jonas Da Silva Garcia <jsgarcia@latam.stefanini.com>
2020-10-21 21:09:56 -0300

    Inicio da criação das telas

---
+++ app/src/main/java/com/example/laboratorio_programacao/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.laboratorio_programacao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
